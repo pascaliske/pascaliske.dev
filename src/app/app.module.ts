acd35d0d79276d8e7567944517829d8e39469a3e
Pascal Iske <info@pascal-iske.de>
2018-01-27 02:44:22 +0100

    feat(app): add automatic progress bar to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core'
 import { BrowserModule, Title } from '@angular/platform-browser'
 import { ServiceWorkerModule } from '@angular/service-worker'
 import { HttpClientModule } from '@angular/common/http'
+import { NgProgressModule } from '@ngx-progressbar/core'
+import { NgProgressHttpModule } from '@ngx-progressbar/http'
+import { NgProgressRouterModule } from '@ngx-progressbar/router'
 
 import { environment } from '../environments/environment'
 
@@ -20,6 +23,9 @@ import { ScrollService } from './services/scroll/scroll.service'
         BrowserModule,
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
         HttpClientModule,
+        NgProgressModule.forRoot(),
+        NgProgressHttpModule,
+        NgProgressRouterModule,
         AppRoutingModule,
         HomePageModule,
         AboutPageModule
