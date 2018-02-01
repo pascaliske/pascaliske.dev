a5c8317b57b61ff26fa5d1c457d2ea1e4cc21f67
Pascal Iske <info@pascal-iske.de>
2018-02-01 17:14:47 +0100

    refactor(navigation): update navigation component

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Router } from '@angular/router'
+import { BreakpointService, Breakpoints } from '../../services/breakpoint/breakpoint.service'
+import 'rxjs/add/operator/takeWhile'
 
 @Component({
     selector: 'cmp-navigation',
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
-    public constructor() {}
+export class NavigationComponent implements OnInit, OnDestroy {
+    public mobile: boolean = false
 
-    public ngOnInit() {}
+    public mobileOpen: boolean = false
+
+    private alive: boolean = true
+
+    public constructor(public router: Router, public breakpointService: BreakpointService) {
+        this.mobile = this.breakpointService.isLarger(Breakpoints.MINI_TABLET) === false
+    }
+
+    public ngOnInit() {
+        this.breakpointService.breakpointChange
+            .takeWhile(() => this.alive)
+            .subscribe(() => this.onBreakpointChange())
+    }
+
+    public ngOnDestroy() {
+        this.alive = false
+    }
+
+    /**
+     * Toggles the state of the mobile navigation.
+     *
+     * @param {boolean} state
+     * @returns {void}
+     */
+    public toggleMobileOpen(state: boolean): void {
+        this.mobileOpen = state
+    }
+
+    /**
+     * Sets the mobile flag according to breakpoints.
+     *
+     * @returns {void}
+     */
+    private onBreakpointChange(): void {
+        this.mobile = !this.breakpointService.isLarger(Breakpoints.MINI_TABLET)
+    }
 }
--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+
+import { NavigationButtonModule } from '../navigation-button/navigation-button.module'
 
 import { NavigationComponent } from './navigation.component'
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule, NavigationButtonModule],
     declarations: [NavigationComponent],
     exports: [NavigationComponent]
 })
