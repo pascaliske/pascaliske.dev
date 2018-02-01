bfaa90946106e0593de16f32a11426e31fb7022a
Pascal Iske <info@pascal-iske.de>
2018-02-01 10:44:06 +0100

    refactor(app): update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,55 @@
 import { Component, HostListener } from '@angular/core'
-import { Title } from '@angular/platform-browser'
 import { BreakpointService } from './services/breakpoint/breakpoint.service'
+import { TitleService } from './services/title/title.service'
 import { ScrollService } from './services/scroll/scroll.service'
 
+/**
+ * AppComponent
+ *
+ * The main component for displaying the complete app.
+ */
 @Component({
     selector: 'cmp-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public title = 'cmp'
-
-    public progress: any = {
-        color: '#2d333d',
-        thick: true,
-        speed: 250
-    }
-
+    /**
+     * Initializes the AppComponent.
+     *
+     * @param {BreakpointService} breakpointService
+     * @param {TitleService} titleService
+     * @param {ScrollService} scrollService
+     * @returns {AppComponent}
+     */
     public constructor(
-        private titleService: Title,
         private breakpointService: BreakpointService,
+        private titleService: TitleService,
         private scrollService: ScrollService
-    ) {}
-
-    public setTitle(title: string) {
-        this.titleService.setTitle(title)
+    ) {
+        this.titleService.setDivider('//')
+        this.titleService.setSuffix('Pascal Iske')
     }
 
+    /**
+     * Pipes the window resize event to the BreakpointService.
+     *
+     * @param {Event} event
+     * @returns {void}
+     */
     @HostListener('window:resize', ['$event'])
-    public onResize(event): void {
+    public onResize(event: Event): void {
         this.breakpointService.handleResize(event)
     }
 
+    /**
+     * Pipes the window scroll event to the ScrollService.
+     *
+     * @param {Event} event
+     * @returns {void}
+     */
     @HostListener('window:scroll', ['$event'])
-    public onScroll(event): void {
+    public onScroll(event: Event): void {
         this.scrollService.handleScroll(event)
     }
 }
