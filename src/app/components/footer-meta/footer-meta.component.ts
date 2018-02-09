5bda594a46fb86cc17ddc92101e0a1dd0d1710eb
Pascal Iske <info@pascal-iske.de>
2018-02-09 19:29:37 +0100

    feat(footer-meta): add sign in and sign out page links to footer-meta component

--- src/app/components/footer-meta/footer-meta.component.ts
+++ src/app/components/footer-meta/footer-meta.component.ts
@@ -1,26 +1,64 @@
 import { Component, OnInit } from '@angular/core'
 import { Router, ActivatedRoute } from '@angular/router'
 
+/**
+ * FooterMetaComponent
+ *
+ * Displays copyright info, legal links and the language switch.
+ */
 @Component({
     selector: 'cmp-footer-meta',
     templateUrl: './footer-meta.component.html',
     styleUrls: ['./footer-meta.component.scss']
 })
 export class FooterMetaComponent implements OnInit {
+    /**
+     * The current language.
+     *
+     * @param {string}
+     */
     public language: string
 
+    /**
+     * The current year.
+     *
+     * @param {number}
+     */
     public year: number = new Date().getFullYear()
 
+    /**
+     * Controls if the observebales in this class shoulb be unsubscribed.
+     *
+     * @param {boolean}
+     */
     private alive: boolean = true
 
+    /**
+     * Initializes the component.
+     *
+     * @param {Router} router
+     * @param {ActivatedRoute} route
+     * @returns {FooterMetaComponent}
+     */
     public constructor(public router: Router, public route: ActivatedRoute) {}
 
-    public ngOnInit() {
+    /**
+     * Fetches the current language and injects it in the component.
+     *
+     * @returns {void}
+     */
+    public ngOnInit(): void {
         this.route.paramMap
             .takeWhile(() => this.alive)
             .subscribe(params => (this.language = params.get('language')))
     }
 
+    /**
+     * Navigates to the current page in the given language.
+     *
+     * @param {string} language - The new language.
+     * @returns {void}
+     */
     public changeLanguage(language: string): void {
         const { path } = this.route.snapshot.url[0]
 
