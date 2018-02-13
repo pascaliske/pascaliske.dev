6becbc8d8994a081a6197b9de360ae3c8af0ddd3
Pascal Iske <info@pascal-iske.de>
2018-02-13 01:31:06 +0100

    feat(copy): add copy component

---
+++ src/app/components/copy/copy.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { CopyComponent } from './copy.component'
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [CopyComponent],
+    exports: [CopyComponent]
+})
+export class CopyModule {}
