8c1c2bf3c90f2735ce0f81d0a479f7b6944c1444
Pascal Iske <info@pascal-iske.de>
2018-02-16 07:51:49 +0100

    fix(env): copy firebase config to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAZgSihxk4D8W5YePzHlnqPGocRfICNNN8',
+        authDomain: 'pascal-iske-de.firebaseapp.com',
+        databaseURL: 'https://pascal-iske-de.firebaseio.com',
+        projectId: 'pascal-iske-de',
+        storageBucket: 'pascal-iske-de.appspot.com',
+        messagingSenderId: '1017340222701'
+    }
 }
