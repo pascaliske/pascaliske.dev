24b9ab06499af21ba64f9aedc82d6548752a29ff
Pascal Iske <info@pascal-iske.de>
2018-03-08 00:14:10 +0100

    refactor(navigation): optimize navigation and navigation button

--- src/app/components/navigation/navigation-button/navigation-button.component.ts
+++ src/app/components/navigation/navigation-button/navigation-button.component.ts
@@ -1,21 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'cmp-navigation-button',
     templateUrl: './navigation-button.component.html',
     styleUrls: ['./navigation-button.component.scss']
 })
-export class NavigationButtonComponent implements OnInit {
+export class NavigationButtonComponent {
     @Input() public state: boolean = false
 
-    @Output() public open: EventEmitter<boolean> = new EventEmitter()
+    @Output() public stateChange: EventEmitter<boolean> = new EventEmitter()
 
     public constructor() {}
-
-    public ngOnInit() {}
-
-    public onClick(): void {
-        this.state = !this.state
-        this.open.emit(this.state)
-    }
 }
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { LanguageService } from '../../services/language/language.service'
 import { BreakpointService, Breakpoints } from '../../services/breakpoint/breakpoint.service'
-import 'rxjs/add/operator/takeWhile'
+import { takeWhile } from 'rxjs/operators'
 
 export interface NavigationItem {
     route: string
@@ -20,39 +20,24 @@ export interface NavigationItem {
 export class NavigationComponent implements OnInit, OnDestroy {
     @Input() public items: Array<NavigationItem> = []
 
-    public language: string
-
     public mobile: boolean = false
 
     public mobileOpen: boolean = false
 
     private alive: boolean = true
 
-    public constructor(public route: ActivatedRoute, public breakpointService: BreakpointService) {
+    public constructor(private breakpointService: BreakpointService) {
         this.mobile = this.breakpointService.isLarger(Breakpoints.MINI_TABLET) === false
     }
 
     public ngOnInit() {
-        this.route.paramMap
-            .takeWhile(() => this.alive)
-            .subscribe(params => (this.language = params.get('language') || 'en'))
-        this.breakpointService.breakpointChange
-            .takeWhile(() => this.alive)
+        this.breakpointService.breakpoint$
+            .pipe(takeWhile(() => this.alive))
             .subscribe(() => this.onBreakpointChange())
     }
 
     public ngOnDestroy() {
         this.alive = false
-    }
-
-    /**
-     * Toggles the state of the mobile navigation.
-     *
-     * @param {boolean} state
-     * @returns {void}
-     */
-    public toggleMobileOpen(state: boolean): void {
-        this.mobileOpen = state
     }
 
     /**
