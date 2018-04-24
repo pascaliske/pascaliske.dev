5833fd09fe4f326727b1ae3f09bdde3ed0678a88
Pascal Iske <info@pascal-iske.de>
2018-04-24 19:19:57 +0200

    fix(env): remove firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    production: true,
-    firebase: {
-        apiKey: 'AIzaSyAZgSihxk4D8W5YePzHlnqPGocRfICNNN8',
-        authDomain: 'pascal-iske-de.firebaseapp.com',
-        databaseURL: 'https://pascal-iske-de.firebaseio.com',
-        projectId: 'pascal-iske-de',
-        storageBucket: 'pascal-iske-de.appspot.com',
-        messagingSenderId: '1017340222701'
-    }
+    production: true
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,3 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: 'AIzaSyAZgSihxk4D8W5YePzHlnqPGocRfICNNN8',
-        authDomain: 'pascal-iske-de.firebaseapp.com',
-        databaseURL: 'https://pascal-iske-de.firebaseio.com',
-        projectId: 'pascal-iske-de',
-        storageBucket: 'pascal-iske-de.appspot.com',
-        messagingSenderId: '1017340222701'
-    }
+    production: false
 }
