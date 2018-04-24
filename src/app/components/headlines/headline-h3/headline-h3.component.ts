09d7cf483480d497ba8cef26da43d948b97c8e63
Pascal Iske <info@pascal-iske.de>
2018-04-25 01:23:19 +0200

    fix(headlines): remove submodules, move logic into headline class and add icons

--- src/app/components/headlines/headline-h3/headline-h3.component.ts
+++ src/app/components/headlines/headline-h3/headline-h3.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component } from '@angular/core'
+import { Headline } from '../headline'
 
 @Component({
     selector: 'cmp-headline-h3',
     templateUrl: './headline-h3.component.html',
     styleUrls: ['./headline-h3.component.scss']
 })
-export class HeadlineH3Component implements OnInit {
-    @Input() public text: string
-
-    public constructor() {}
-
-    public ngOnInit() {}
-}
+export class HeadlineH3Component extends Headline {}
--- src/app/components/headlines/headline-h4/headline-h4.component.ts
+++ src/app/components/headlines/headline-h4/headline-h4.component.ts
@@ -1,12 +1,9 @@
-import { Component, Input } from '@angular/core'
+import { Component } from '@angular/core'
+import { Headline } from '../headline'
 
 @Component({
     selector: 'cmp-headline-h4',
     templateUrl: './headline-h4.component.html',
     styleUrls: ['./headline-h4.component.scss']
 })
-export class HeadlineH4Component {
-    @Input() public text: string
-
-    public constructor() {}
-}
+export class HeadlineH4Component extends Headline {}
