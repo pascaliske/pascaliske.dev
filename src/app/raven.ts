18d41196b19fb13bf4f1554b2ee56d6df5c4f246
Pascal Iske <info@pascal-iske.de>
2018-08-08 00:28:27 +0200

    chore(sentry): improve sentry logging with release version and environment tags

--- src/app/raven.ts
+++ src/app/raven.ts
@@ -1,16 +1,31 @@
-import { ErrorHandler } from '@angular/core'
-import * as Raven from 'raven-js'
+import { Injectable, ErrorHandler } from '@angular/core'
+import Raven from 'raven-js'
 import { environment } from '../environments/environment'
 
+// configures sentry's raven library
 if (environment.production) {
-    Raven.config(environment.sentryDsn).install()
+    Raven.config(environment.sentry.dsn, environment.sentry.options).install()
 }
 
+/**
+ * Injectable error handler for sentry.
+ */
+@Injectable()
 export class RavenErrorHandler implements ErrorHandler {
+    /**
+     * Handles any error.
+     *
+     * @param {any} error
+     * @returns {void}
+     */
     public handleError(error: any): void {
+        // log error to sentry in production
         if (environment.production) {
-            Raven.captureException(error)
+            Raven.captureException(error.originalError || error)
+            return
         }
+
+        // re-throw error in dev mode
         throw error
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
+import pkg from '../../package.json'
+
 export const environment = {
     production: false,
-    sentryDsn: 'https://da20741e2b93497caba31ff22a6290bc@sentry.io/1200530',
+    sentry: {
+        dsn: 'https://da20741e2b93497caba31ff22a6290bc@sentry.io/1200530',
+        options: {
+            environment: 'development',
+            release: `v${pkg.version}`,
+            debug: true,
+        },
+    },
 }
