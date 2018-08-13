3aa2a7fb0a1b1dfe0182f56b8bef0564744c59e7
Pascal Iske <info@pascal-iske.de>
2018-08-13 15:57:41 +0200

    feat(imprint-page): add contents and update styling

--- src/app/pages/site/imprint-page/imprint-page.module.ts
+++ src/app/pages/site/imprint-page/imprint-page.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core'
 import { SharedModule } from '../../../shared/shared.module'
 import { PageHeaderModule } from '../../../components/page-header/page-header.module'
 import { SectionModule } from '../../../components/section/section.module'
+import { HeadlinesModule } from '../../../components/headlines/headlines.module'
+import { LinkModule } from '../../../components/link/link.module'
+import { CopyModule } from '../../../components/copy/copy.module'
 import { QuickContactModule } from '../../../components/quick-contact/quick-contact.module'
 import { ImprintPageComponent } from './imprint-page.component'
 
 @NgModule({
-    imports: [SharedModule, PageHeaderModule, SectionModule, QuickContactModule],
+    imports: [
+        SharedModule,
+        PageHeaderModule,
+        SectionModule,
+        HeadlinesModule,
+        LinkModule,
+        CopyModule,
+        QuickContactModule,
+    ],
     declarations: [ImprintPageComponent],
 })
 export class ImprintPageModule {}
