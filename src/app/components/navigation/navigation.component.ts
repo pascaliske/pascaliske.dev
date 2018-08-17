897b53fc542485a8dc736d075b551d476d745bf8
Pascal Iske <info@pascal-iske.de>
2018-08-17 09:45:52 +0200

    refactor(navigation): optimize navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core'
+import { modifiers } from '@pascaliske/html-helpers'
 import { takeWhile } from 'rxjs/operators'
 import { LanguageService } from '../../shared/language/language.service'
 import { BreakpointService, Breakpoints } from '../../shared/breakpoint/breakpoint.service'
 
+/**
+ * Representation of an navigation item.
+ */
 export interface NavigationItem {
     route: string
     label: string
@@ -18,38 +22,74 @@ export interface NavigationItem {
     styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit, OnDestroy {
+    /**
+     * Array of navigation items.
+     *
+     * @param {Array<NavigationItem>}
+     */
     @Input()
     public items: Array<NavigationItem> = []
 
+    /**
+     * Represents the current breakpoint state of the navigation.
+     *
+     * @param {boolean} mobile
+     */
     public mobile: boolean = false
 
+    /**
+     * Represents the current open/close state of the mobile navigation.
+     *
+     * @param {boolean} mobileOpen
+     */
     public mobileOpen: boolean = false
 
+    /**
+     * Lifecycle control property.
+     *
+     * @param {boolean} alive
+     */
     private alive: boolean = true
 
+    /**
+     * Initializes the component.
+     *
+     * @param {LanguageService} languageService
+     * @param {BreakpointService} breakpointService
+     */
     public constructor(
         public languageService: LanguageService,
         private breakpointService: BreakpointService,
-    ) {
-        this.mobile = this.breakpointService.isLarger(Breakpoints.MINI_TABLET) === false
+    ) {}
+
+    /**
+     * Setup logic.
+     *
+     * @returns {void}
+     */
+    public ngOnInit(): void {
+        this.breakpointService.breakpoint$.pipe(takeWhile(() => this.alive)).subscribe(() => {
+            this.mobile = this.breakpointService.isLarger(Breakpoints.MINI_TABLET) === false
+        })
     }
 
-    public ngOnInit() {
-        this.breakpointService.breakpoint$
-            .pipe(takeWhile(() => this.alive))
-            .subscribe(() => this.onBreakpointChange())
-    }
-
-    public ngOnDestroy() {
+    /**
+     * Teardown logic.
+     *
+     * @returns {void}
+     */
+    public ngOnDestroy(): void {
         this.alive = false
     }
 
     /**
-     * Sets the mobile flag according to breakpoints.
+     * Returns the component classes.
      *
-     * @returns {void}
+     * @returns {string}
      */
-    private onBreakpointChange(): void {
-        this.mobile = !this.breakpointService.isLarger(Breakpoints.MINI_TABLET)
+    public get classes(): string {
+        return modifiers('cmp-navigation', {
+            open: this.mobile && this.mobileOpen,
+        })
     }
 }
