15a0742c33ce91bd6c82343fa68824510943be3e
Pascal Iske <info@pascal-iske.de>
2018-08-31 17:25:36 +0200

    chore(angular): replace given constants during builds

--- ngw.config.ts
+++ ngw.config.ts
@@ -3,6 +3,7 @@ import { NormalizedBrowserBuilderSchema } from '@angular-devkit/build-angular'
 import { join } from 'path'
 import { sync } from 'glob'
 import { Configuration } from 'webpack'
+import * as ReplacePlugin from 'webpack-plugin-replace'
 import * as PurifyCSSPlugin from 'purifycss-webpack'
 import * as VisualizerPlugin from 'webpack-visualizer-plugin'
 
@@ -12,12 +13,20 @@ export interface WebpackOptions<T = NormalizedBrowserBuilderSchema> {
     options: T
 }
 
+// tslint:disable-next-line
+const pkg = require('./package.json')
 const command = process.argv[2].toLowerCase()
 
 export default function(config: Configuration, options: WebpackOptions) {
     if (command === 'build') {
         config.resolve.alias['marked'] = 'marked/marked.min'
         config.plugins.push(
+            new ReplacePlugin({
+                include: ['environment.ts'],
+                values: {
+                    SENTRY_RELEASE: `v${pkg.version}`,
+                },
+            }),
             new PurifyCSSPlugin({
                 paths: sync(join(__dirname, '**/*.html')),
             }),
