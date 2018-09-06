5bdb1f5a4f43a52ba1bdb9967620ece77b0683d8
Pascal Iske <info@pascal-iske.de>
2018-09-06 12:04:18 +0200

    chore(angular): integrate webpack dashboard with serve and build commands

--- ngw.config.ts
+++ ngw.config.ts
@@ -3,6 +3,7 @@ import { NormalizedBrowserBuilderSchema } from '@angular-devkit/build-angular'
 import { join } from 'path'
 import { sync } from 'glob'
 import { Configuration } from 'webpack'
+import * as DashboardPlugin from 'webpack-dashboard/plugin'
 import * as ReplacePlugin from 'webpack-plugin-replace'
 import * as PurifyCSSPlugin from 'purifycss-webpack'
 import * as VisualizerPlugin from 'webpack-visualizer-plugin'
@@ -18,9 +19,14 @@ const pkg = require('./package.json')
 const command = process.argv[2].toLowerCase()
 
 export default function(config: Configuration, options: WebpackOptions) {
+    if (command === 'serve') {
+        config.plugins.push(new DashboardPlugin())
+    }
+
     if (command === 'build') {
         config.resolve.alias['marked'] = 'marked/marked.min'
         config.plugins.push(
+            new DashboardPlugin(),
             new ReplacePlugin({
                 include: ['environment.ts'],
                 values: {
