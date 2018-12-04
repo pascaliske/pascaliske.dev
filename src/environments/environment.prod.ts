a65105c90b86570fff5cbe0a12bbb0d44b5394be
Pascal Iske <info@pascal-iske.de>
2018-12-04 07:52:10 +0100

    fix(env): use custom domain as cfn base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     release: 'RELEASE',
-    cfnBaseUrl: 'https://us-central1-pascal-iske-de.cloudfunctions.net',
+    cfnBaseUrl: '/cfns',
     cookieDomain: 'pascal-iske.de',
     sentry: {
         dsn: 'https://da20741e2b93497caba31ff22a6290bc@sentry.io/1200530',
