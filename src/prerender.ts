bac94811923bd91e71a1102b214056de636adbb3
Pascal Iske <info@pascal-iske.de>
2018-12-07 09:35:29 +0100

    chore(package): use external prerendering helper

--- src/prerender.ts
+++ src/prerender.ts
@@ -1,41 +1,18 @@
 import 'reflect-metadata'
 import 'zone.js/dist/zone-node'
-import chalk from 'chalk'
 import { enableProdMode } from '@angular/core'
 import { renderModuleFactory } from '@angular/platform-server'
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader'
-import { join, dirname } from 'path'
-import { readFile, ensureDir, writeFile } from 'fs-extra'
+import { create } from '@pascaliske/ngx-prerenderer'
 import { routes } from './app/app-routing.module'
 
 enableProdMode()
 
-async function prerender(): Promise<void> {
-    const dist = file => join(process.cwd(), 'dist', 'app', file)
-    const paths = routes.map(route => route.path)
-    const index = (await readFile(dist('index.html'), 'utf8')).toString()
+// tslint:disable-next-line
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist/server/main')
+const prerender = create(renderModuleFactory, provideModuleMap)
 
-    // tslint:disable-next-line
-    const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist/server/main')
-
-    for (const path of paths) {
-        if (!path.includes('*')) {
-            const file = dist(`${path || 'index'}.html`)
-            const rendered = await renderModuleFactory(AppServerModuleNgFactory, {
-                url: path,
-                document: index,
-                extraProviders: [provideModuleMap(LAZY_MODULE_MAP)],
-            })
-
-            await ensureDir(dirname(file))
-            await writeFile(file, rendered)
-
-            console.log(`    rendered ${chalk.cyan('/' + path)} -> ${chalk.cyan(file)}`)
-        }
-    }
-}
-
-prerender()
+prerender(routes, AppServerModuleNgFactory, LAZY_MODULE_MAP)
     .then(() => {
         console.log('==> done')
     })
