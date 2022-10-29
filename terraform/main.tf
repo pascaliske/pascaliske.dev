7644ffb9f2c6b574b5b46f4de630ed82b5faf5e5
Pascal Iske <info@pascaliske.dev>
2022-10-29 22:01:57 +0200

    feat(terraform): implement dns management via terraform
    feat(terraform): add more dns records

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "remote" {
+    organization = "pascaliske"
+
+    workspaces {
+      name = "pascaliske-dev"
+    }
+  }
+
+  required_version = ">= 1.3.3"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.26.0"
+    }
+  }
+}
