511226005c875cdb81c020fe555c756972636d7c
Pascal Iske <info@pascaliske.dev>
2023-07-24 14:15:40 +0200

    feat(terraform): implement http redirect rules for legacy content pages

--- terraform/rules.tf
+++ terraform/rules.tf
@@ -9,3 +9,39 @@ resource "cloudflare_page_rule" "redirect_www" {
     }
   }
 }
+
+resource "cloudflare_page_rule" "redirect_about" {
+  zone_id = data.cloudflare_zone.zone.id
+  target  = "www.${local.domain}/about"
+
+  actions {
+    forwarding_url {
+      status_code = 301
+      url         = "https://${local.domain}/home"
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "redirect_imprint" {
+  zone_id = data.cloudflare_zone.zone.id
+  target  = "www.${local.domain}/imprint"
+
+  actions {
+    forwarding_url {
+      status_code = 301
+      url         = "https://${local.domain}/legal-notice"
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "redirect_privacy" {
+  zone_id = data.cloudflare_zone.zone.id
+  target  = "www.${local.domain}/privacy"
+
+  actions {
+    forwarding_url {
+      status_code = 301
+      url         = "https://${local.domain}/legal-notice"
+    }
+  }
+}
