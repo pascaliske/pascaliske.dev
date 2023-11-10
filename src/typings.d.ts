5a0e4fe5aa385d7deba1ec16ed5f946bb588a9c0
Pascal Iske <info@pascaliske.dev>
2023-11-10 11:02:10 +0100

    feat: allow manual tracking of custom events using plausible

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -19,3 +19,15 @@ export const enum Tags {
     TERRAFORM = 'Terraform',
     TYPESCRIPT = 'TypeScript/JavaScript',
 }
+
+/**
+ * Manually trigger custom events for Plausible.
+ */
+declare let plausible: (
+    event: string,
+    options?: {
+        props?: Record<string, string>
+        revenue?: { currency: string; amount: string | number }
+        callback?: () => void
+    },
+) => void
