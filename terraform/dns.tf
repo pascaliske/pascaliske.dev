374b5864b65650bc1a4900c82ea9ac9ad95c41a4
Pascal Iske <info@pascaliske.dev>
2024-08-16 11:08:20 +0200

    refactor(terraform): use new content argument for cloudflare record resources

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "production_ipv4" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "A"
   name    = "@"
-  value   = "192.0.2.1"
+  content = "192.0.2.1"
   proxied = true
 }
 
@@ -11,7 +11,7 @@ resource "cloudflare_record" "production_ipv6" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "AAAA"
   name    = "@"
-  value   = "2001:db8::"
+  content = "2001:db8::"
   proxied = true
 }
 
@@ -19,7 +19,7 @@ resource "cloudflare_record" "production_www_ipv4" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "A"
   name    = "www"
-  value   = "192.0.2.1"
+  content = "192.0.2.1"
   proxied = true
 }
 
@@ -27,7 +27,7 @@ resource "cloudflare_record" "production_www_ipv6" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "AAAA"
   name    = "www"
-  value   = "2001:db8::"
+  content = "2001:db8::"
   proxied = true
 }
 
@@ -36,7 +36,7 @@ resource "cloudflare_record" "next_ipv4" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "A"
   name    = "next"
-  value   = "192.0.2.1"
+  content = "192.0.2.1"
   proxied = true
 }
 
@@ -44,7 +44,7 @@ resource "cloudflare_record" "next_ipv6" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "AAAA"
   name    = "next"
-  value   = "2001:db8::"
+  content = "2001:db8::"
   proxied = true
 }
 
@@ -53,7 +53,7 @@ resource "cloudflare_record" "mail" {
   zone_id  = data.cloudflare_zone.zone.id
   type     = "MX"
   name     = "@"
-  value    = "tempel.uberspace.de"
+  content  = "tempel.uberspace.de"
   priority = 0
 }
 
@@ -62,7 +62,7 @@ resource "cloudflare_record" "bimi" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = "default._bimi"
-  value   = "v=BIMI1; l=https://${var.DOMAIN}/assets/icons/bimi.svg;"
+  content = "v=BIMI1; l=https://${var.DOMAIN}/assets/icons/bimi.svg;"
 }
 
 # dmarc
@@ -70,7 +70,7 @@ resource "cloudflare_record" "dmarc" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = "_dmarc"
-  value   = "v=DMARC1; p=quarantine; rua=mailto:info@${var.DOMAIN}"
+  content = "v=DMARC1; p=quarantine; rua=mailto:info@${var.DOMAIN}"
 }
 
 # dkim, uberspace mail
@@ -78,7 +78,7 @@ resource "cloudflare_record" "dkim" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = "uberspace._domainkey"
-  value   = "v=DKIM1; t=s; n=core; p=${var.DKIM_PUBLIC_KEY}"
+  content = "v=DKIM1; t=s; n=core; p=${var.DKIM_PUBLIC_KEY}"
 }
 
 # spf
@@ -86,7 +86,7 @@ resource "cloudflare_record" "spf" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = var.DOMAIN
-  value   = "v=spf1 include:spf.uberspace.de include:relay.mailchannels.net ~all"
+  content = "v=spf1 include:spf.uberspace.de include:relay.mailchannels.net ~all"
 }
 
 # mail lockdown
@@ -94,7 +94,7 @@ resource "cloudflare_record" "mail_lockdown" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = "_mailchannels"
-  value   = "v=mc1 cfid=${var.DOMAIN}"
+  content = "v=mc1 cfid=${var.DOMAIN}"
 }
 
 # openpgpkey
@@ -102,7 +102,7 @@ resource "cloudflare_record" "openpgpkey" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = "openpgpkey"
-  value   = "noop"
+  content = "noop"
 }
 
 # google
@@ -110,7 +110,7 @@ resource "cloudflare_record" "google" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = var.DOMAIN
-  value   = "google-site-verification=${var.GOOGLE_VERIFICATION_TOKEN}"
+  content = "google-site-verification=${var.GOOGLE_VERIFICATION_TOKEN}"
 }
 
 # have-i-been-pwned
@@ -118,5 +118,5 @@ resource "cloudflare_record" "have_i_been_pwned" {
   zone_id = data.cloudflare_zone.zone.id
   type    = "TXT"
   name    = var.DOMAIN
-  value   = "have-i-been-pwned-verification=${var.HAVE_I_BEEN_PWNED_VERIFICATION_TOKEN}"
+  content = "have-i-been-pwned-verification=${var.HAVE_I_BEEN_PWNED_VERIFICATION_TOKEN}"
 }
