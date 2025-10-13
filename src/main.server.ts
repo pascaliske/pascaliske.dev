03f174481627868b0c766921caf4f1062e0a2460
Pascal Iske <info@pascaliske.dev>
2025-10-13 21:54:02 +0200

    feat: upgrade angular to v20.3

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,10 +1,10 @@
 import { ApplicationRef } from '@angular/core'
-import { bootstrapApplication } from '@angular/platform-browser'
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { serverConfig } from './app/app.config.server'
 
-export const bootstrap: () => Promise<ApplicationRef> = () => {
-    return bootstrapApplication(AppComponent, serverConfig)
+export const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> => {
+    return bootstrapApplication(AppComponent, serverConfig, context)
 }
 
 export default bootstrap
