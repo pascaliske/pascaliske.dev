934b2bc4c12f4cb7c6ce58cb7b7bd75ebdeee17f
Pascal Iske <info@pascaliske.dev>
2025-12-05 11:55:27 +0100

    refactor: switch to new template syntax

--- src/app/components/headline/headline.component.ts
+++ src/app/components/headline/headline.component.ts
@@ -1,10 +1,10 @@
 import { Component, input } from '@angular/core'
-import { NgSwitch, NgSwitchCase, NgIf, NgTemplateOutlet } from '@angular/common'
+import { NgTemplateOutlet } from '@angular/common'
 
 @Component({
     selector: 'cmp-headline',
     templateUrl: './headline.component.html',
-    imports: [NgSwitch, NgSwitchCase, NgIf, NgTemplateOutlet],
+    imports: [NgTemplateOutlet],
 })
 export class HeadlineComponent {
     public readonly level = input<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>('h1')
